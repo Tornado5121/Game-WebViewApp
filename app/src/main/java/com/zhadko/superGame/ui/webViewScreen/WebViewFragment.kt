fdb5a8a92683d569a7b3e5278b09d7fc2f78f3ca
Roman Zhadko <zhadkoroman@gmail.com>
2023-02-18 06:41:25 +0200

    handle navigation back of webView

--- app/src/main/java/com/zhadko/superGame/ui/webViewScreen/WebViewFragment.kt
+++ app/src/main/java/com/zhadko/superGame/ui/webViewScreen/WebViewFragment.kt
@@ -2,6 +2,8 @@ package com.zhadko.superGame.ui.webViewScreen
 
 import android.os.Bundle
 import android.view.View
+import android.webkit.WebViewClient
+import androidx.activity.OnBackPressedCallback
 import androidx.lifecycle.lifecycleScope
 import com.zhadko.superGame.base.BaseFragment
 import com.zhadko.superGame.databinding.LayoutFragmentWebViewBinding
@@ -22,9 +24,21 @@ class WebViewFragment : BaseFragment<LayoutFragmentWebViewBinding>(
 
     private fun setupView() {
         val settings = binding.myWebView.settings
+        binding.myWebView.webViewClient = WebViewClient()
         settings.javaScriptEnabled = true
         settings.domStorageEnabled = true
         settings.databaseEnabled = true
+        requireActivity().onBackPressedDispatcher.addCallback(
+            viewLifecycleOwner,
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    if (binding.myWebView.canGoBack()) {
+                        binding.myWebView.goBack()
+                    } else {
+                        requireActivity().finish()
+                    }
+                }
+            })
     }
 
     private fun loadWebView(savedInstanceState: Bundle?) {
@@ -38,4 +52,6 @@ class WebViewFragment : BaseFragment<LayoutFragmentWebViewBinding>(
             }
         }
     }
+
+
 }
